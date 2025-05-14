658eb5af5d4ef62844007283329e7dec609a3eda
Xavi <21989858+xaskii@users.noreply.github.com>
2025-05-14 14:05:54 -0400

    separate out configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,78 @@
+{ config, pkgs, ... }:
+
+{
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    vim
+    neovim
+    nvimpager
+    htop
+    ffmpeg
+    btop
+    git
+    wget
+
+    ripgrep
+    vivid
+    gh
+    neovim
+    tlrc
+    stow
+    fzf
+    fd
+    ruff
+    duf
+    jq
+    curlie
+    xh
+    eza
+    bandwhich
+    just
+    biome
+    fastfetch
+    zoxide
+    imagemagick
+    bat
+
+    # tuis
+    dua
+    lazygit
+    delta
+    yazi
+    tmux
+    zellij
+
+    nil
+    nixfmt-rfc-style
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  # yeah idk idgaf
+  users.users."spring".uid = 501;
+  users.users."spring".shell = pkgs.fish;
+  users.knownUsers = [ "spring" ];
+
+  programs.zsh.enable = true;
+  programs.fish.enable = true;
+  programs.nix-index.enable = true; # fixes the random error with wrong commands
+
+  nix.optimise.automatic = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
+
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = config.rev or config.dirtyRev or null;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 6;
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+}
--- flake.nix
+++ flake.nix
@@ -14,100 +14,22 @@
   };
 
   outputs =
-    inputs@{
+    {
       self,
       nix-darwin,
       nixpkgs,
       lix-module,
       ...
-    }:
-    let
-      configuration =
-        { pkgs, ... }:
-        {
-          # List packages installed in system profile. To search by name, run:
-          # $ nix-env -qaP | grep wget
-          environment.systemPackages = with pkgs; [
-            vim
-            neovim
-            nvimpager
-            htop
-            ffmpeg
-            btop
-            git
-            wget
-
-            ripgrep
-            vivid
-            gh
-            neovim
-            tlrc
-            stow
-            fzf
-            fd
-            ruff
-            duf
-            jq
-            curlie
-            xh
-            eza
-            bandwhich
-            just
-            biome
-            fastfetch
-            zoxide
-            imagemagick
-            bat
-
-            # tuis
-            dua
-            lazygit
-            delta
-            yazi
-
-            nil
-            nixfmt-rfc-style
-          ];
-
-          nixpkgs.config.allowUnfree = true;
-
-          nix.settings.experimental-features = [
-            "nix-command"
-            "flakes"
-          ];
-
-          # yeah idk idgaf
-          users.users."spring".uid = 501;
-          users.users."spring".shell = pkgs.fish;
-          users.knownUsers = [ "spring" ];
-
-
-          programs.zsh.enable = true;
-          programs.fish.enable = true;
-          programs.nix-index.enable = true; # fixes the random error with wrong commands
-
-          nix.optimise.automatic = true;
-          security.pam.services.sudo_local.touchIdAuth = true;
-
-          # Set Git commit hash for darwin-version.
-          system.configurationRevision = self.rev or self.dirtyRev or null;
-
-          # Used for backwards compatibility, please read the changelog before changing.
-          # $ darwin-rebuild changelog
-          system.stateVersion = 6;
-
-          # The platform the configuration will be used on.
-          nixpkgs.hostPlatform = "aarch64-darwin";
-        };
-    in
+    }@inputs:
     {
       # Build darwin flake using:
       # $ darwin-rebuild build --flake .#life
       # if you break some bullshit
       # nix run nix-darwin/master#darwin-rebuild -- switch
       darwinConfigurations."life" = nix-darwin.lib.darwinSystem {
+        specialArgs = { inherit inputs; };
         modules = [
-          configuration
+          ./configuration.nix
           lix-module.nixosModules.default
         ];
       };
