2862b1bfac5071346512b8a434144e23ff00d3ba
Xavi Simpson <21989858+xaskii@users.noreply.github.com>
2025-11-21 21:07:20 -0500

    Try to get home-manager working but cry instead

--- hosts/four/default.nix
+++ hosts/four/default.nix
@@ -1,10 +1,22 @@
 lib: {
   config = lib.darwinSystem' ({ config, pkgs, ... }: {
-    # Set primary user configuration
+    networking.hostName = "four";
+
     system.primaryUser = "spring";
 
-    # Configure hostname
-    networking.hostName = "four";
+    users.users.spring = {
+      name = "spring";
+      home = "/Users/spring";
+    };
+
+    home-manager.users.spring = {
+      programs.nushell.enable = true;
+
+      home = {
+        stateVersion = "25.05";
+        homeDirectory = config.users.users.spring.home;
+      };
+    };
 
     nixpkgs.hostPlatform = "aarch64-darwin";
     system.stateVersion = 6;
