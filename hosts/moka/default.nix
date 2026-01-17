dc4956fa1d08b73b38bfe5ec72a3458415ec034d
Xavi Simpson <21989858+xaskii@users.noreply.github.com>
2026-01-17 00:30:36 -0500

    Add new host: moka

---
+++ hosts/moka/default.nix
@@ -0,0 +1,40 @@
+lib: {
+  class = "nixos";
+  config = lib.nixosSystem' ({ config, pkgs, ... }: {
+    imports = [
+      ./hardware.nix
+    ];
+
+    type = "server";
+
+    boot.loader.systemd-boot.enable = false;
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "/dev/sda";
+    boot.loader.efi.canTouchEfiVariables = false;
+
+    networking.hostName = "moka";
+
+    users.users.spring = {
+      isNormalUser = true;
+      description = "spring";
+      extraGroups = [
+        "wheel"
+      ];
+      shell = pkgs.fish;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO7ISHEtrXPczITdF5vOOJGSqmsTBG7nkRccQJzgolhG xaskii"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAEDheq2VO1MROtfrod8szRaqaTrWX1A5riZOuzgD1zu spring@four"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINcBV7sB6ZBziiJy1Z3q8zNDOXRIBqjw4aoYln8UN6QY XAVIIIII"
+      ];
+    };
+    users.users.root.openssh.authorizedKeys.keys =
+      config.users.users.spring.openssh.authorizedKeys.keys;
+
+    home-manager.users.spring.home = {
+      stateVersion  = "24.11";
+      homeDirectory = "/home/spring";
+    };
+
+    system.stateVersion = "24.11";
+  });
+}
