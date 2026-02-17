68152da58b006f3b8131110cc55ec3e28305a1ec
Xavi Simpson <21989858+xaskii@users.noreply.github.com>
2026-02-17 16:07:22 -0500

    temp: patch broken nvidia drivers

--- modules/linux/graphics.nix
+++ modules/linux/graphics.nix
@@ -1,10 +1,27 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 lib.mkIf config.isDesktop {
   # Graphics configuration
   hardware.graphics.enable = true;
 
   hardware.nvidia.open = true;
+  # temp patch since out of tree nvidia drivers don't work with new kernel
+  hardware.nvidia.package =
+      let
+        base = config.boot.kernelPackages.nvidiaPackages.latest;
+        cachyos-nvidia-patch = pkgs.fetchpatch {
+          url = "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/nvidia/nvidia-utils/kernel-6.19.patch";
+          sha256 = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
+        };
 
+        # Patch the appropriate driver based on config.hardware.nvidia.open
+        driverAttr = if config.hardware.nvidia.open then "open" else "bin";
+      in
+      base
+      // {
+        ${driverAttr} = base.${driverAttr}.overrideAttrs (oldAttrs: {
+          patches = (oldAttrs.patches or [ ]) ++ [ cachyos-nvidia-patch ];
+        });
+      };
   services.xserver.videoDrivers = [ "nvidia" ];
 }
