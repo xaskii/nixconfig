d61afa0bbefcd0f29e45865ddc6de796903a8728
Xavi Simpson <21989858+xaskii@users.noreply.github.com>
2026-02-25 14:57:34 -0500

    unagi/linux: Add Adwaita font settings

--- modules/linux/fonts.nix
+++ modules/linux/fonts.nix
@@ -4,12 +4,30 @@ lib.mkIf config.isDesktop {
   fonts = {
     enableDefaultPackages = true;
     packages = [
+      pkgs.adwaita-fonts
       pkgs.noto-fonts
       pkgs.noto-fonts-color-emoji
       pkgs.noto-fonts-cjk-sans
       pkgs.iosevka-bin
       pkgs.jetbrains-mono
     ];
-    fontconfig.useEmbeddedBitmaps = true; # for emojis on firefox
+    fontconfig = {
+      antialias = true;
+      hinting = {
+        enable = true;
+        style = "slight";
+      };
+      subpixel = {
+        rgba = "rgb";
+        lcdfilter = "default";
+      };
+      useEmbeddedBitmaps = true; # for emojis on firefox
+      defaultFonts = {
+        sansSerif = [ "Adwaita Sans" "Noto Sans CJK SC" ];
+        serif = [ "Noto Serif" "Noto Serif CJK SC" ];
+        monospace = [ "JetBrains Mono" "Noto Sans Mono CJK SC" ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
   };
 }
